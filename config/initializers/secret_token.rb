48e45064f51ac3fec5f31c1fa0cc2b753db146ea
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-06 17:13:23 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NTouch::Application.config.secret_key_base = '39c9c09f864f2a00824d738c681d76bedb202e0b5ecb05cfc733ef2e3d82657e1211d64da0e0c17d6329e5183bd17b1b386ee9a55ee02ec384d56dbb418b378c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NTouch::Application.config.session_store :cookie_store, key: '_nTouch_session'
