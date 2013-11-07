20bfe2b62f36316ad4024a2fdf4c5d8ba4be54a8
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-07 09:15:48 -0600

    factory girl set up with passing tests for user, friends, and events

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,4 +1,10 @@
 class Friend < ActiveRecord::Base
+	attr_accessible :first_name, :last_name, :email, :birthday, :phone_number, :photo_name
+
+	validates_presence_of :first_name, :last_name, :email, :phone_number
+  	validates :email, format: { with: /\w+@\w+\.\w{2,3}/ }
+  	validates :email, :phone_number, uniqueness: true
+
 	belongs_to :user
 	has_many :events
 
--- spec/factories.rb
+++ spec/factories.rb
@@ -5,6 +5,7 @@ FactoryGirl.define do
     last_name "Larson"
     email "alexisboss@heckyes.biz"
     birthday "July 31, 1984"
+    phone_number "1234567890"
 
   end
 
@@ -13,6 +14,22 @@ FactoryGirl.define do
     last_name "Rules!"
     email nil
     birthday "July 31, 1984"
+    phone_number "1234567890"
   end
 
+  factory :friend do
+    first_name "Fred"
+    last_name "Bob"
+    email "fred@bob.com"
+    birthday "October 25, 1987"
+    phone_number "1234567890"
+  end
+
+  factory :invalid_friend do
+    first_name "Fred"
+    last_name "Bob"
+    email nil
+    birthday "October 25, 1987"
+    phone_number "1234567890"
+  end
 end
