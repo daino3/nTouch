aad25ba0d7da92a32fd5993579451875b9b92aa3
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-08 09:39:25 -0600

    updating friend model in this branch

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,12 +1,9 @@
 class Friend < ActiveRecord::Base
-	attr_accessible :first_name, :last_name, :email, :birthday, :phone_number, :photo_name
-
-	validates_presence_of :first_name, :last_name, :email, :phone_number
-  	validates :email, format: { with: /\w+@\w+\.\w{2,3}/ }
-  	validates :email, :phone_number, uniqueness: true
-
 	belongs_to :user
 	has_many :events
 
-  attr_accessible :first_name, :last_name, :birthday
+	attr_accessible :first_name, :last_name, :birthday
+
+	validates_presence_of :first_name, :last_name, :birthday
+
 end
