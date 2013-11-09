6f331eda0106af8e3509f3af23d402c8c77a369b
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-08 21:15:19 -0600

    fixed push::deleted send_text rake, and deleted twilio stuff out of omniauth

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,9 +1,9 @@
 class Sms
 
 	# this is being referenced by the ApplicationController(not really though)
-def send_text_message(phone_number, friend)
-	@phone = phone
-	@friend = Friend.find(friend)
+def send_text_message(phone_number)
+	@phone_number = phone_number
+	# @friend = Friend.find(friend)
 
 		twilio_sid = ENV['TWILIO_SID']
 		twilio_token = ENV['TWILIO_TOKEN']
@@ -12,7 +12,7 @@ def send_text_message(phone_number, friend)
 		@client = Twilio::REST::Client.new twilio_sid, twilio_token
 		@client.account.sms.messages.create(
 			:from => "+1#{twilio_phone_number}",
-      		:to => "+1#{@phone}",
+      		:to => "+1#{@phone_number}",
       		:body => "YYYYYYYYYEEEEEEEEEEEEEESSSSSSSSSSSSSSSSSS!!!!!11"
     	) #interpolate the user phone number
 	end
