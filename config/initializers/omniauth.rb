94a18f106d91c3b2972e42e9307df3b1e8d9fbbd
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-09 00:33:49 -0600

    fixed user show page and deleted yaml file information from initializers

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
-env_config = YAML.load_file("config/facebook.yaml")
-
-env_config.each do |key, value|
-  ENV[key]=value
-end
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
 end
