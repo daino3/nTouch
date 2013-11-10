7be8ced13205ef8c91eecb76e63abeec5ff4ec66
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-10 12:36:38 -0600

    incorporated validation and error prompts for unique friend uids and can now search and add friends to user's account

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,14 +1,16 @@
 class FriendsController < ApplicationController
 
   def index
+    @graph = Koala::Facebook::API.new(current_user.oauth_token)
+    @friends = @graph.get_connections('me', 'friends')
   end
 
   def create
     if current_user.friends.count < 10
       new_friend = Friend.new(new_friend_params)
-      if Friend.exists?(uid: new_friend.uid)
+      if current_user.friends.find_by_uid(new_friend.uid)
         flash[:notice]="#{new_friend.first_name} is already included in your list. Please select someone who is not."
-        redirect_to user_path(current_user)
+        redirect_to user_friends_path(current_user)
       else
         new_friend.save
         current_user.friends << new_friend
