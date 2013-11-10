4e6034cafc64c3eb274213751f6aa2eebf83eb29
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-10 12:56:14 -0600

    deleted booleans out of event model

--- db/migrate/20131109012431_addtextandemailbooleantoeventmodel.rb
+++
@@ -1,6 +0,0 @@
-class Addtextandemailbooleantoeventmodel < ActiveRecord::Migration
-  def change
-    add_column :events, :text, :boolean, default: false
-    add_column :events, :email, :boolean, default: false
-  end
-end
