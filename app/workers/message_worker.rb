6dbeb30b480d2be16bc8a7387a50a5e3ca1dd495
Jonathan Howden <jonathanhowden@gmail.com>
2013-11-14 10:30:43 -0600

    updated sendgrid to deliver emails

--- app/workers/message_worker.rb
+++ app/workers/message_worker.rb
@@ -7,15 +7,15 @@ class MessageWorker
     if event.notificationtype == Event::NOTIFICATION_TYPE[0]
       Sms.new.send_text_message(user.phone_number, event)
       if event.eventtype == Event::EVENT_TYPE[2]
-        UserMailer.annual_email(user, event)
+        UserMailer.annual_email(user, event).deliver
       elsif event.eventtype == Event::EVENT_TYPE[1]
-        UserMailer.frequent_email(user, event)
+        UserMailer.frequent_email(user, event).deliver
       end
     elsif event.notificationtype == Event::NOTIFICATION_TYPE[2]
       if event.eventtype == Event::EVENT_TYPE[2]
-        UserMailer.annual_email(user, event)
+        UserMailer.annual_email(user, event).deliver
       elsif event.eventtype == Event::EVENT_TYPE[1]
-        UserMailer.anniversary_email(user, event)
+        UserMailer.anniversary_email(user, event).deliver
       end
     elsif event.notificationtype == Event::NOTIFICATION_TYPE[1]
       Sms.new.send_text_message(user.phone_number, event)
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 require 'redis'
 
-uri = URI.parse(ENV["REDISTOGO_URL"] || 'redis://localhost:6379/')
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/")
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
